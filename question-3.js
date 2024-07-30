d4e4b92a41cedb8de8849ebb4317625f1b69ad3c
Panpak Apesariyayo <ingpanpak@gmail.com>
2024-07-30 10:29:43 +0700

    Algro

--- question-3.js
+++ question-3.js
@@ -1,20 +1,23 @@
 /*
 What is the Big O notation for Function findStudentById . Explain your reason.
-Answer:
+Answer: the Big O of this function is O(n) because the times this function working is depends on the length of array. 
 
 
 */
 
 /* 
 What is the Big O notation for Function findProductPrice. Explain your reason.
-Answer:
+Answer: the Big O of this function is O(Log n) because the times this function working is depends on the length of array like
+Function findStudentById but the differant is if I have more length in array the time complexity not increase equal in Function findStudentById
+because working by if it dont find the result you want it will compare the result you want and result it get of that loop and then it will cut
+the half of it.The reason make this function not checking every object in array.  
 
 
 */
 
 /* 
 Which function is more efficient and why?
-Answer:
+Answer: Function findProductPrice because the time complexity less than Function findStudentById
 
 
 */
